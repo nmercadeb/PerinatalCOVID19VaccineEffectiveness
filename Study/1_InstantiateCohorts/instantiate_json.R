b1e0ca2bbefc8e33cfcb1e1a96482794e00ceb56
Núria Mercadé-Besora <61558739+nmercadeb@users.noreply.github.com>
2024-07-10 21:07:23 +0100

    Update instantiate_json.R

--- Study/1_InstantiateCohorts/instantiate_json.R
+++ Study/1_InstantiateCohorts/instantiate_json.R
@@ -129,6 +129,7 @@ json_cohort_counts <- cdm[[medications_table_name]] %>%
                          by = "cohort_definition_id"))  %>%
   union_all(cdm[[ps_covariates_table_name]] %>%
               settings() %>%
+              select(cohort_definition_id, cohort_name) %>%
               inner_join(cdm[[ps_covariates_table_name]] %>%
                            cohort_count() %>%
                            mutate(cohort_group = "ps_covariates"),
